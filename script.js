70b08ebb76751f813ee034b2be547bb30bbb036d
Cale Adrian Lacson <keyladrian7@gmail.com>
2019-02-15 18:09:17 +0800

    Iniatially added card 3 and some shitty js script

---
+++ script.js
@@ -0,0 +1,9 @@
+function show(){
+    var back = document.getElementById('back');
+    back.style.display = 'block';
+}
+
+function hide(){
+    var back = document.getElementById('back');
+    back.style.display = 'none';
+}
